0983ae34975fb1365aa88d54f52bd4eec8d8b68d
codewithtabish <kashisultan099@gmail.com>
2026-07-19 21:33:28 +0500

    chore: setup shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime error similique et magni
+      placeat iusto, dolor voluptatibus nesciunt nobis autem voluptate necessitatibus odio, quisquam
+      culpa suscipit. Nam rem adipisci voluptas.
+    </div>
+  );
 };
 
 export default HomePage;
