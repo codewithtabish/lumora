06eb204735f4b6684ccabdb8d2e06105baea53bf
codewithtabish <kashisultan099@gmail.com>
2026-07-20 19:01:53 +0500

    feat: add Clerk-based authentication redirect on app entry

--- src/app/(pages)/homebar/page.tsx
+++ src/app/(pages)/homebar/page.tsx
@@ -4,12 +4,14 @@ import React from "react";
 const HomeScreenPage = () => {
   return (
     <AppShell>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus minima, nostrum
-      rerum, impedit eaque, nobis ea culpa reiciendis voluptas consectetur dolores sunt distinctio!
-      Consequatur dolore facere voluptatibus ut. Tempore. Lorem ipsum dolor sit amet consectetur
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex voluptatem repudiandae eum
+      assumenda, deserunt facilis eligendi molestias! Saepe blanditiis in animi dignissimos eligendi
+      fuga corrupti, impedit facere quae atque ipsum! Lorem ipsum dolor sit amet consectetur
       adipisicing elit. Voluptatum ducimus minima, nostrum rerum, impedit eaque, nobis ea culpa
       reiciendis voluptas consectetur dolores sunt distinctio! Consequatur dolore facere
-      voluptatibus ut. Tempore.
+      voluptatibus ut. Tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+      ducimus minima, nostrum rerum, impedit eaque, nobis ea culpa reiciendis voluptas consectetur
+      dolores sunt distinctio! Consequatur dolore facere voluptatibus ut. Tempore.
     </AppShell>
   );
 };
