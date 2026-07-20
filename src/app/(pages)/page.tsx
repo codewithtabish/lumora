5d5a1276dce2e288a961e063c18648172cf629f5
codewithtabish <kashisultan099@gmail.com>
2026-07-20 16:11:38 +0500

    feat: added test data

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -21,7 +21,16 @@ const HomePage = () => {
         tempora soluta, quas distinctio atque earum, incidunt animi voluptatum minima! Lorem, ipsum
         dolor sit amet consectetur adipisicing elit. Delectus mollitia molestiae eaque unde
         doloremque numquam illo cupiditate temporibus sint maxime tempora soluta, quas distinctio
-        atque earum, incidunt animi voluptatum minima!
+        atque earum, incidunt animi voluptatum minima! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nisi debitis tempore corporis mollitia, voluptates impedit aliquam, earum
+        ex sapiente voluptatibus aliquid dignissimos ut laboriosam quis in repellendus, omnis
+        accusantium quo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora rem qui
+        accusantium numquam iusto ex aut quis ipsam blanditiis totam debitis, doloribus alias et
+        nisi rerum quod porro dolore quas. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Tempora rem qui accusantium numquam iusto ex aut quis ipsam blanditiis totam debitis,
+        doloribus alias et nisi rerum quod porro dolore quas. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Tempora rem qui accusantium numquam iusto ex aut quis ipsam blanditiis
+        totam debitis, doloribus alias et nisi rerum quod porro dolore quas.
         <Button>Checks</Button>
         <Link href={"/onboarding"}>
           <Button>Onboading</Button>
