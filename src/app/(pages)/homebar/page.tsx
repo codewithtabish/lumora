67d21394fe8c147209b4ab652f5bb830aeee6ce3
codewithtabish <kashisultan099@gmail.com>
2026-07-20 18:33:26 +0500

    feat: optimize login screen for responsive PWA layout

---
+++ src/app/(pages)/homebar/page.tsx
@@ -0,0 +1,17 @@
+import { AppShell } from "@/components/(app)/(common)/app-shell";
+import React from "react";
+
+const HomeScreenPage = () => {
+  return (
+    <AppShell>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus minima, nostrum
+      rerum, impedit eaque, nobis ea culpa reiciendis voluptas consectetur dolores sunt distinctio!
+      Consequatur dolore facere voluptatibus ut. Tempore. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Voluptatum ducimus minima, nostrum rerum, impedit eaque, nobis ea culpa
+      reiciendis voluptas consectetur dolores sunt distinctio! Consequatur dolore facere
+      voluptatibus ut. Tempore.
+    </AppShell>
+  );
+};
+
+export default HomeScreenPage;
